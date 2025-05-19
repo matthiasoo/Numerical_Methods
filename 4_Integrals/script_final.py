6bb80c8afc0c5fd918feda0ec20454d68e7f5b98
matthiasoo <makky.x.55@gmail.com>
2025-05-19 21:44:18 +0200

    Simpson with and without wage

--- 4_Integrals/script_final.py
+++ 4_Integrals/script_final.py
@@ -50,8 +50,20 @@ def wxfx(x, function_id):
     except ZeroDivisionError:
         return 0
 
+# version = 0 - wersja bez wagi (używa fx)
+# version = 1 - wersja z wagą (używa wxfx)
+def function_type(x, function_id, version) :
+    if version == 0 :
+        return fx(x, function_id)
+    elif version == 1 :
+        return wxfx(x, function_id)
+    else:
+        raise ValueError("Nieprawidłowy wybór wersji!")
+
 # wzór Simpsona dla złożonej kwadratury Newtona-Cotesa opartej na 3 węzłach
-def simpson(function_id, a, b, eps) :
+# version = 0 - wersja bez wagi (używa fx)
+# version = 1 - wersja z wagą (używa wxfx)
+def simpson(function_id, a, b, eps, version) :
     num_range = b - a # długość przedziału
     subrange = 1 # początkowa liczba podprzedziałów
     integral = 0 # zmienna przechowująca wartość całki
@@ -60,20 +72,26 @@ def simpson(function_id, a, b, eps) :
         subrange *= 2
         s_len = num_range / subrange # długość jednego podprzedziału
         prev_integral = integral # zapis poprzedniej wartości całki żeby później porównać ją z nową
-        integral = wxfx(a, function_id) + wxfx(b, function_id) # nowa wartość całki jako suma wartości funkcji w(x)f(x)
-                                                               # w punktach granicznych a i b
-
+        integral = function_type(a, function_id, version) + function_type(b, function_id, version) # nowa wartość całki jako suma wartości funkcji f(x)
+                                                           # w punktach granicznych a i b
+        # przechodzimy przez punkty wewn. przedziałów
         for i in range(1, subrange // 2) :
-            integral += 4 * wxfx(a + (2 * i - 1) * s_len, function_id)
-            integral += 2 * wxfx(a + (2 * i) * s_len, function_id)
+            integral += 4 * function_type(a + (2 * i - 1) * s_len, function_id, version) # wartości w środku każdego podprzedziału
+                                                                       # 4 to waga dla punktów środkowych
+            integral += 2 * function_type(a + (2 * i) * s_len, function_id, version) # wartości na granicach między podpredziałami
+                                                                   # 2 to waga dla punktów pośrednich
 
-        integral *= s_len / 3
+        integral *= s_len / 3 # zgodnie z wzorem Simpsona
+        # różnica < eps i wiecej niz 1 podprzedzial
         if abs(prev_integral - integral) < eps and subrange > 1 :
             break
 
     return integral
 
-# wzór Simpsona na mniejszych podprzedziałach
+# wzór Simpsona na przedziale [-1, 1]
+# dzielimy na podprzedziały (szczegolnie blisko koncow)
+# i zbliżamy się do +-1,
+# ponieważ funkcja wagowa jest nieskończona w +-1
 def simpson_lim(function_id, eps) :
     result = 0
 
@@ -81,8 +99,9 @@ def simpson_lim(function_id, eps) :
     a = 0
     b = 0.5
 
+    # zbliżamy się do 1
     while True :
-        integral = simpson(function_id, a, b, eps)
+        integral = simpson(function_id, a, b, eps, 1) # używamy wersji Simpsona z wagą
         result += integral
         a = b
         b += ((1 - b) / 2)
@@ -93,8 +112,9 @@ def simpson_lim(function_id, eps) :
     a = -0.5
     b = 0
 
+    # zbliżamy się do -1
     while True :
-        integral = simpson(function_id, a, b, eps)
+        integral = simpson(function_id, a, b, eps, 1) # używamy wersji Simpsona z wagą
         result += integral
         b = a
         a -= ((1 - abs(b)) / 2)
@@ -107,9 +127,9 @@ def simpson_lim(function_id, eps) :
 def gauss_chebyshev(function_id, nodes_num) :
     integral = 0
     for i in range(1, nodes_num + 1) :
-        w = PI / nodes_num
-        node = -np.cos((2 * i - 1) * PI / (2 * nodes_num))
-        integral += w * fx(node, function_id)
+        w = PI / nodes_num # obliczamy wage
+        node = -np.cos((2 * i - 1) * PI / (2 * nodes_num)) #obliczenie węzłów za pomocą cosinusa (literatury)
+        integral += w * fx(node, function_id) # mnożenie wartości funkcji przez wagę
     return integral
 
 def main():
@@ -123,12 +143,20 @@ def main():
     function_id = int(input("\nFunkcja: "))
     eps = float(input("\nPodaj dokładność: "))
 
-    print("\nMETODA NEWTONA-COTESA\n---------------------")
+    a = float(input("\nPodaj a: "))
+    b = float(input("\nPodaj b: "))
+
+    print(f"WZÓR SIMPSONA BEZ WAGI NA PRZEDZIALE [{a};{b}]\n--------------------------")
+    print(simpson(function_id, a, b, eps, 0))
+
+    print("\nMETODA NEWTONA-COTESA\n--------------------------")
     print("Wynik:", simpson_lim(function_id, eps))
 
-    print("\nMETODA GAUSSA-CZEBYSZEWA\n------------------------")
+    print("\nMETODA GAUSSA-CZEBYSZEWA\n--------------------------")
     for nodes in range(2, 6):
         print(f"Liczba węzłów: {nodes}")
         print(f"Wynik: {gauss_chebyshev(function_id, nodes)}\n")
+
+
 
 main()
